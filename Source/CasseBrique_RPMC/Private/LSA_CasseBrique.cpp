c98f718a8dda7b0e778fcb5d1798db71ae3bd214
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-09-11 20:59:56 -0400

    [ADD] - Add LevelScriptActor for the main LVL

--- Source/CasseBrique_RPMC/Private/LSA_CasseBrique.cpp
+++ Source/CasseBrique_RPMC/Private/LSA_CasseBrique.cpp
@@ -3,3 +3,20 @@
 
 #include "LSA_CasseBrique.h"
 
+#include "Camera/CameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+void ALSA_CasseBrique::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the actor that handles control for the local player.
+	APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
+
+	if(IsValid(MainCamera))
+	{
+		//Cut instantly to our actor on begin play.
+		OurPlayerController->SetViewTarget(MainCamera);
+		OurPlayerController->SetShowMouseCursor(false);
+	}
+}
--- Source/CasseBrique_RPMC/Public/LSA_CasseBrique.h
+++ Source/CasseBrique_RPMC/Public/LSA_CasseBrique.h
@@ -14,4 +14,12 @@ class CASSEBRIQUE_RPMC_API ALSA_CasseBrique : public ALevelScriptActor
 {
 	GENERATED_BODY()
 	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+public:
+	
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	ACameraActor* MainCamera;
 };
