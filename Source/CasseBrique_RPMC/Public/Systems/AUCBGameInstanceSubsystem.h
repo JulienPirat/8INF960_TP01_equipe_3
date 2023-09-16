94f0cb7ea40939360c3043b025a17148f2389ec5
GuillaumeClerc <clercguigui62@gmail.com>
2023-09-16 13:26:41 -0400

    Add Tiny Plugins and start menu
    Add plugins made by Tiny/Benj
    And put his start menu in the project

---
+++ Source/CasseBrique_RPMC/Public/Systems/AUCBGameInstanceSubsystem.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerInput.h"
+#include "UObject/Object.h"
+#include "AUCBGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASSEBRIQUE_RPMC_API UAUCBGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FInputActionKeyMapping MappingContext;
+};
