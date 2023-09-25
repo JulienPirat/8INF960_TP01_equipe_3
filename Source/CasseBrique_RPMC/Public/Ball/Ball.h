1f70ccaa08e65f42cb82fecf3fbf3af6cfdbf6a3
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-09-25 19:50:59 -0400

    Condition de victoire

--- Source/CasseBrique_RPMC/Public/Ball/Ball.h
+++ Source/CasseBrique_RPMC/Public/Ball/Ball.h
@@ -72,7 +72,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent);
 
 private:
 	UPROPERTY(BlueprintGetter=GetSpeed, BlueprintSetter=SetSpeed)
